f8aeeba2580e067d29fa72a21d67e8a6b4cccdde
D-Atharv <ad298suncity@gmail.com>
2024-12-17 23:04:55 +0530

    feat: fixed basic auth changes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,13 +7,30 @@ import (
 	"net/http"
 	"os"
 	database "server/internal/db"
+	// "server/internal/models"
 	"server/internal/routes"
 	"server/internal/webSocket"
+
+	"github.com/rs/cors"
 )
 
 func main() {
 
 	database.ConnectDB()
+
+	// err := database.DB.AutoMigrate(&models.User{}) //uncomment for migration 
+	// if err != nil {
+	// 	log.Fatalf("Failed to migrate database: %v", err)
+	// }
+
+	fmt.Println("Database migration completed!")
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello world using golang")
@@ -28,16 +45,19 @@ func main() {
 		port = "8080"
 	}
 
+	handler := c.Handler(router)
+
 	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatal("Failed to start server:", err)
 	}
 
 	fmt.Printf("Server running on port %s\n", port)
-	if err := http.Serve(listener, nil); err != nil {
+	if err := http.Serve(listener, handler); err != nil {
 		log.Fatal("Server error:", err)
 	}
 
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(port, handler))
+
 
 }
