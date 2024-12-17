85ee4a22465585b35320fe1bb04e1a7cafe8f58c
D-Atharv <ad298suncity@gmail.com>
2024-12-17 19:30:59 +0530

    feat: database connected successfully

--- internal/db/database.go
+++ internal/db/database.go
@@ -1,1 +1,43 @@
 package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"github.com/joho/godotenv"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Println("No .env file found, using default values")
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslMode := os.Getenv("SSL_MODE")
+
+	if host == "" || user == "" || password == "" || dbName == "" || port == "" {
+		log.Fatal("Database environment variables are not set properly")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbName, port, sslMode,
+	)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	fmt.Println("Database connected successfully!")
+}
--- internal/models/movement.go
+++ internal/models/movement.go
@@ -1,6 +1,7 @@
 package models
 
 type Position struct {
+	ID     uint    `gorm:"primaryKey"`
 	UserID string  `json:"user_id" gorm:"not null;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;references:ID"` 
 	X      float64 `json:"x"`
 	Y      float64 `json:"y"`
